9b6397cfc2e46617b15c00fdf36211850d3e6e22
dongZc <a358673817@163.com>
2018-03-01 22:08:47 +0800

    add

--- tetris-server/src/main/java/com/learn/teris/websocket/TextWebSocketFrameHandler.java
+++ tetris-server/src/main/java/com/learn/teris/websocket/TextWebSocketFrameHandler.java
@@ -1,11 +1,14 @@
 package com.learn.teris.websocket;
 
 import com.learn.teris.http.HttpRequestHandler;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+import java.util.HashMap;
 
 /**
  * 功能说明: <br>
@@ -16,6 +19,8 @@ import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
     private final ChannelGroup group;
+    private HashMap<Channel, String> channelName = new HashMap<>();
+    private int count;
 
     public TextWebSocketFrameHandler(ChannelGroup group) {
         this.group = group;
@@ -36,7 +41,7 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
             // 如果是握手升级连接， 删除掉原本的Http处理器
             ctx.pipeline().remove(HttpRequestHandler.class);
             // 通知所有连接的WebSocket客户端有新的客户端连接上
-            group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + "joined"));
+            //group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + "joined"));
             group.add(ctx.channel());
         } else {
             super.userEventTriggered(ctx, evt);
@@ -48,4 +53,32 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
         // 增加消息的引用计数, 并将它写到ChannelGroup中的所有已连接客户端
         group.writeAndFlush(msg.retain());
     }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        count++;
+        Channel channel = ctx.channel();
+        group.add(channel);
+        channelName.put(channel, "client" + count);
+        group.writeAndFlush(new TextWebSocketFrame(channelName.get(channel) + " 加入聊天"));
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        group.writeAndFlush(new TextWebSocketFrame(channelName.get(channel) + " 离开聊天"));
+    }
+
+//    @Override
+//    public void channelActive(ChannelHandlerContext ctx) throws Exception { // (5)
+//        Channel channel = ctx.channel();
+//        group.writeAndFlush(new TextWebSocketFrame("Client " + channel.remoteAddress() + " 在线"));
+//    }
+//
+//    @Override
+//    public void channelInactive(ChannelHandlerContext ctx) throws Exception { // (6)
+//        Channel channel = ctx.channel();
+//        group.writeAndFlush(new TextWebSocketFrame("Client " + channel.remoteAddress() + " 掉线"));
+//    }
+
 }
